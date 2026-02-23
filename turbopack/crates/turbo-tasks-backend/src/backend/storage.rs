10668fbdb434387df25528fea44d34122e24af04
Luke Sandberg <lukeisandberg@gmail.com>
2026-02-23 12:45:55 -0800

    fix min shard size

--- turbopack/crates/turbo-tasks-backend/src/backend/storage.rs
+++ turbopack/crates/turbo-tasks-backend/src/backend/storage.rs
@@ -104,7 +104,7 @@ impl Storage {
         let (modified_shards, snapshot_shards) = if track_modifications {
             (shard_amount, SNAPSHOT_SHARDS)
         } else {
-            (1, 1)
+            (1, 2)
         };
 
         Self {
